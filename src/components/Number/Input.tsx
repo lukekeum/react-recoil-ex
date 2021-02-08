f9e34fb461baa32ffb3d9a26d9dc28789da54c13
lukekeum <lukekeum11@gmail.com>
2021-02-08 18:16:20 +0900

    Style Input and Button in Number Route

--- src/components/Number/Input.tsx
+++ src/components/Number/Input.tsx
@@ -1,10 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { css } from '@emotion/react';
 import { useRecoilState } from 'recoil';
 import { NumberData } from '../../atom/number';
 
 export default function Input() {
   const [number, setNumber] = useRecoilState(NumberData);
+  const InputRef = useRef<HTMLInputElement>(null);
 
   const onChangeInput = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,6 +22,11 @@ export default function Input() {
   const onClickButton = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
       e.preventDefault();
+
+      if (!InputRef.current?.value) {
+        alert('Non number found');
+        return;
+      }
 
       const { current_num, increase_size } = number;
 
@@ -40,12 +46,31 @@ export default function Input() {
         placeholder='Put number here'
         value={number.increase_size}
         onChange={onChangeInput}
+        ref={InputRef}
       />
-      <button onClick={onClickButton}>Add</button>
+      <button css={NumberButton} onClick={onClickButton}>
+        Add
+      </button>
     </div>
   );
 }
 
 const NumberInput = css({
   outline: 'none',
+  border: '1px solid #bfbfbf',
+  fontSize: '1.5rem',
+  padding: '0.5rem 1rem',
+  borderRadius: '3px',
+  marginRight: '1.5rem',
+});
+
+const NumberButton = css({
+  outline: 'none',
+  border: 'none',
+  fontSize: '1.5rem',
+  background: '#14b886',
+  cursor: 'pointer',
+  color: 'white',
+  padding: '0.5rem 1rem',
+  borderRadius: '3px',
 });
--- src/components/Number/Title.tsx
+++ src/components/Number/Title.tsx
@@ -1,8 +1,13 @@
 import { useRecoilState } from 'recoil';
+import { css } from '@emotion/react';
 import { NumberData } from '../../atom/number';
 
 export default function Title() {
   const [number] = useRecoilState(NumberData);
 
-  return <h1>Number: {number.current_num}</h1>;
+  return <h1 css={TitleCSS}>{number.current_num}</h1>;
 }
+
+const TitleCSS = css({
+  fontSize: '3rem',
+});
